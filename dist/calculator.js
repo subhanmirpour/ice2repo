e25c1c794f8e418945573321442daaa6bd791d6f
Subhan Mirpour <subhanmirpour08@gmail.com>
2024-03-12 19:42:02 -0400

    init commit

---
+++ dist/calculator.js
@@ -0,0 +1,20 @@
+"use strict";
+let add = (a, b) => {
+    return a + b;
+};
+let subtract = (a, b) => {
+    return a - b;
+};
+let multiply = (a, b) => {
+    return a * b;
+};
+let divide = (a, b) => {
+    return a / b;
+};
+module.exports = {
+    add: add,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide
+};
+//# sourceMappingURL=calculator.js.map
---
+++ src/calculator.ts
@@ -0,0 +1,18 @@
+let add = (a: number, b: number) => {
+    return a + b;
+};
+let subtract = (a: number, b: number) => {
+    return a - b;
+};
+let multiply = (a: number, b: number) => {
+    return a * b;
+};
+let divide = (a: number, b: number) => {
+    return a / b;
+};
+module.exports = {
+    add: add,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide
+};
